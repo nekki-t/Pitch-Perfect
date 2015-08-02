72d98187ef993f60a5c8455002857216aab3c1dc
nekki t <nekki.t.0317@gmail.com>
2015-08-02 09:58:23 +0900

    echo and reverb implemented

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -15,6 +15,10 @@ class PlaySoundsViewController: UIViewController {
     // MARK: - Local Variables
     //####################################################
     var audioPlayer: AVAudioPlayer!
+    var echoedPlayer: AVAudioPlayer!
+    
+    let wetDryDegree:Float = 100
+    
     var receivedAudio: RecordedAudio!
     var audioEngine: AVAudioEngine!
     var audioFile: AVAudioFile!
@@ -27,6 +31,9 @@ class PlaySoundsViewController: UIViewController {
         
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
+        
+        echoedPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        
         
         audioEngine = AVAudioEngine()
         audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
@@ -51,10 +58,44 @@ class PlaySoundsViewController: UIViewController {
         playAudioWithVariablePitch(1000)
     }
     
+    // Darth Vader Button Tapped
     @IBAction func playDarthvaderAudio(sender: UIButton) {
         playAudioWithVariablePitch(-1000)
     }
     
+    // Echo Button Tapped
+    @IBAction func playEchoAudio(sender: UIButton) {
+        // first runner
+        readyForPlayNew()
+        playAudio(1.0)
+        
+        // echo runner
+        let delay:NSTimeInterval = 0.1//100ms
+        var playtime:NSTimeInterval
+        playtime = echoedPlayer.deviceCurrentTime + delay
+        
+        echoedPlayer.stop()
+        echoedPlayer.currentTime = 0.0
+        echoedPlayer.playAtTime(playtime)
+    }
+    
+    // Reverb Button Tapped
+    @IBAction func playReverbAudio(sender: UIButton) {
+        readyForPlayNew()
+        
+        var reverb:AVAudioUnitReverb = AVAudioUnitReverb()
+        reverb.loadFactoryPreset(.LargeHall2)
+        reverb.wetDryMix = wetDryDegree
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        audioEngine.attachNode(reverb)
+        
+        audioEngine.connect(audioPlayerNode, to: reverb, format: nil)
+        audioEngine.connect(reverb, to: audioEngine.outputNode, format: nil)
+        
+        playWithAudioEngine(audioPlayerNode)
+    }
     
     // Stop Button Tapped
     @IBAction func stopAudio(sender: UIButton) {
@@ -78,6 +119,8 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.stop()
         audioEngine.stop()
         audioEngine.reset()
+        
+        echoedPlayer.stop()
     }
     
     // Play with Pitch
@@ -94,6 +137,11 @@ class PlaySoundsViewController: UIViewController {
         audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
         audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
         
+        playWithAudioEngine(audioPlayerNode)
+    }
+    
+    // play node with Audio Engine
+    func playWithAudioEngine(audioPlayerNode: AVAudioPlayerNode!) {
         audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
         audioEngine.startAndReturnError(nil)
         
